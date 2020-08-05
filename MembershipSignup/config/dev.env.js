7bcbe9114c3cc54076fa598fec52ae3201e42ef5
Karoline Berge <karoline.berge@makingwaves.com>
2020-08-05 17:57:07 +0200

    Tried to change link

--- MembershipSignup/config/dev.env.js
+++ MembershipSignup/config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ENDPOINT: '"http://localhost:5000/"'
+  API_ENDPOINT: '"https://athleticasignup.azurewebsites.net/"'
 })
