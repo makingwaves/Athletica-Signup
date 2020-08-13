dace0dca5fa7e693baf049a6b4f9a8a7fcb67d65
Ida Motzfeldt <ida.motzfeldt@makingwaves.com>
2020-08-13 10:59:10 +0200

    Change pic from svg to png

--- src/frontend/MembershipSignup/src/pages/EndPage.vue
+++ src/frontend/MembershipSignup/src/pages/EndPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <BaseProgressBar :active="4" />
-    <img src="../assets/pics/end_image.svg" />
+    <img src="../assets/pics/end_image.png" />
     <p id="end">Kvittering og mer info er sendt til {{ email }}</p>
     <router-link to="/">
       <BaseButton classType="sec" text="Ferdig"></BaseButton>
